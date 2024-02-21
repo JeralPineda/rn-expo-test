50b117fba2687a9c70cfd2196c35316a7921767b
Jeral Pineda <jeral.cris@gmail.com>
2024-02-20 22:17:22 -0600

    First commit and initial configuration

---
+++ src/app/_layout.tsx
@@ -0,0 +1,11 @@
+import { SessionProvider } from "@/context/ctx";
+import { Slot } from "expo-router";
+
+export default function Root() {
+  // Set up the auth context and render our layout inside of it.
+  return (
+    <SessionProvider>
+      <Slot />
+    </SessionProvider>
+  );
+}
